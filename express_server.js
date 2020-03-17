ba2b72b98b3f1113bbd8a5ea05f9f912ce109264
Junsung Park <zeipar@gmail.com>
2020-03-17 03:28:47 +0000

    fixed random alphanumeric generator and changed post request to redirect to shortened URL

--- express_server.js
+++ express_server.js
@@ -3,11 +3,18 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const generateRandomString = () => {
-  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+  let result = '';
+  const length = 6;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -42,10 +49,9 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  // let ranString = generateRandomString();
-  urlDatabase[generateRandomString()] = req.body.longURL;
-  res.send("OK");
+  const shortStr = generateRandomString();
+  urlDatabase[shortStr] = req.body.longURL;
+  res.redirect(`/urls/${shortStr}`);
 })
 
 app.listen(PORT, () => {
